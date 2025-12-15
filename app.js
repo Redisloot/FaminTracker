ed987b64fd556566f47992dafe72cd55a6a4c31f
Redisloot <redisloot555@gmail.com>
2025-12-14 22:11:58 -0500

    initial files. this may work hopefully

---
+++ app.js
@@ -0,0 +1,26 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbxhDLVEo9bx1E7fwFnqgyeUp-41ef6KQWoEQxX2ZhvCcY9IqS_eUFjTSuMUthlj3hj2/exec";
+
+fetch(API_URL)
+  .then(r => r.json())
+  .then(res => renderTree(res.data));
+
+function renderTree(data) {
+  const container = document.getElementById("tree-container");
+  data.forEach(person => {
+    const div = document.createElement("div");
+    div.className = "node";
+    div.textContent = person.full_name;
+    div.onclick = () => openModal(person);
+    container.appendChild(div);
+  });
+}
+
+function openModal(person) {
+  document.getElementById("name").textContent = person.full_name;
+  document.getElementById("bio").textContent = person.bio;
+  document.getElementById("photo").src = person.photo_url;
+  document.getElementById("modal").classList.remove("hidden");
+}
+
+document.getElementById("close").onclick = () =>
+  document.getElementById("modal").classList.add("hidden");
